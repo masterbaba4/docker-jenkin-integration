83b2076260102cd888570935fea5ed20a01a517a
masterbaba4 <masterbaba4@gmail.com>
2022-10-31 13:29:55 +0530

    first commit

---
+++ src/main/java/com/baba/sample_dev/SampleDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.baba.sample_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDevopsApplication.class, args);
+	}
+
+}
