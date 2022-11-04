b0974f9b595c833f75cafe19a02d884137a81e4b
masterbaba4 <masterbaba4@gmail.com>
2022-11-04 17:33:43 +0530

    mapping page commit

--- src/main/java/com/baba/sample_dev/MasterBabaController.java
+++ src/main/java/com/baba/sample_dev/MasterBabaController.java
@@ -1,15 +1,15 @@
-package com.baba.sample_dev;
+// package com.baba.sample_dev;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class MasterBabaController {
-    @GetMapping
-(path = "/")
-	public String helloWorld() {
-		return "{\"message\":\"Baba is live now\"}";
-	}
-}
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// @RestController
+// public class MasterBabaController {
+//     @GetMapping
+// (path = "/")
+// 	public String helloWorld() {
+// 		return "{\"message\":\"Baba is live now\"}";
+// 	}
+// }
 
 
--- src/main/java/com/baba/sample_dev/SampleDevopsApplication.java
+++ src/main/java/com/baba/sample_dev/SampleDevopsApplication.java
@@ -2,12 +2,21 @@ package com.baba.sample_dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SampleDevopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleDevopsApplication.class, args);
+		@RestController class MasterBabaController{
+			@GetMapping
+			(path = "/")
+	public String helloWorld() {
+		return "{\"message\":\"Baba is live now\"}";
+	}
+		}
 		// System.out.println("Baba is live now");
 	}
 
